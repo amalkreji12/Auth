8bf42ad2677ce9592bab656edb825f659dd081cd
Amal K Reji <amalreji45.amal@gmail.com>
2024-09-07 23:09:39 +0530

    Initial Commit

--- routes/user.js
+++ routes/user.js
@@ -4,28 +4,48 @@ var userhelper = require('../controllers/user-helpers');
 
 
 router.get('/',((req,res)=>{
-    res.render('home')
+    if(req.session.user){
+        res.redirect('login');
+    }else{
+        res.render('home')
+    }
+    // res.render('home')
 }));
 
 router.get('/login',((req,res)=>{
     res.render('login')
-}))
+}));
 
 router.get('/signup',((req,res)=>{
-    res.render('signup')
-}))
+    if(req.session.user){
+        res.render('signup-confirm')
+    }else{
+        res.render('signup')
+    }
+    
+}));
 
 router.post('/signup',((req,res)=>{
     userhelper.doSignUp(req.body).then((response)=>{
-        console.log(response);  
+        //console.log(response);  
+        req.session.user = response;
+        req.session.user.loggedIn = true;
+        res.redirect('signup-confirm')
     })
     .catch((err)=>{
+        console.log(err);
         res.status(500).send('Signup failed');
     })
-    req.session.user = response;
-    req.session.user.loggedIn = true;
-    res.render('userhome')
-}))
+}));
+
+router.get('/signup-confirm',(req,res)=>{
+    if(req.session.user){
+        res.render('signup-confirm')
+    }else{
+        res.redirect('signup')
+    }
+    console.log(req.session.user);
+})
 
 
 
